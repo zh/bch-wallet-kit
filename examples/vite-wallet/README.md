47e8def88173b012095eb949aff77567a494bf81
zhware <zh@zhware.net>
2025-01-20 22:21:36 +0900

    Add wallet example

---
+++ examples/vite-wallet/README.md
@@ -0,0 +1,3 @@
+# Vite Wallet
+
+Very simple wallet, using `bch-wallet-kit`.
---
+++ examples/vite-wallet/src/App.jsx
@@ -0,0 +1,29 @@
+import { useAtom } from 'jotai';
+import { walletConnectedAtom } from 'bch-wallet-kit';
+import { LoadScript, Mnemonic, Options, Wallet } from 'bch-wallet-kit';
+import 'bch-wallet-kit/dist/BchWalletKit.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './confirm.css';
+import './App.css';
+
+const App = () => {
+  const [walletConnected] = useAtom(walletConnectedAtom);
+
+  return (
+    <div className="app-container">
+      <LoadScript scriptSrc="/minimal-slp-wallet.min.js" />
+      <div className="app-title">BCH Wallet</div>
+      {!walletConnected && (
+       <>
+        <Mnemonic />
+        <Options />
+       </>
+      )}
+       <Wallet />
+       <ToastContainer position="top-right" autoClose={3000} />
+    </div>
+  );
+};
+
+export default App;
